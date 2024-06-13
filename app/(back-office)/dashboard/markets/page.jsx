74396cc9f5bd75e7bd10d3551bb47ed3dd7605ee
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2024-06-14 00:36:00 +0530

    Project Setup

---
+++ app/(back-office)/dashboard/markets/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       <h1>markets</h1> 
+    </div>
+  )
+}
+
+export default page
---
+++ app/(back-office)/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function layout() {
+  return (
+    <div className='flex'>
+    {{/* sidebar */}}
+    
+    {{/* mainbody */}}
+      
+    </div>
+  )
+}
